6df1d10c7709301d04f0b4698be10f881fd3e56b
mohate233 <alshabrawy378@gmail.com>
2025-12-19 10:45:24 +0200

    take

--- Register.php
+++ Register.php
@@ -2,8 +2,8 @@
 include "db.php";
 $firstname = $_POST['firstname'];
 $Lastname = $_POST['lastname'];
-$email = $_POST['emailAddress'];
-$password = $_POST['SetApassword'];
+$email = $_POST['email'];
+$password = $_POST['password'];
 
 $password = password_hash($password, PASSWORD_DEFAULT);
 $conn=query("INSERT INTO users (firstname, lastname, email,password)
