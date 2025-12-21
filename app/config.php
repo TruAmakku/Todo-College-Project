94cb5dee464cdd84cf0832f7380ed785a0e8108f
mohate233 <alshabrawy378@gmail.com>
2025-12-21 13:33:53 +0200

    take

--- app/config.php
+++ app/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', 'ss44mm55');
+define('DB_PASS', '');
 define('DB_NAME', 'todo_app');
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
